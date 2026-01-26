95628eb03b84e772b3cc33d6c8b26b831e821a18
amoshood06 <amoshood06@gmail.com>
2026-01-26 10:56:59 +0100

    ueuiwuieweywey

--- header-one.php
+++ header-one.php
@@ -123,8 +123,8 @@ try {
     </div>
 
     <div class="flex items-center gap-2 sm:gap-6">
-      <a href="cart.php" class="hidden sm:block relative text-gray-400 border p-2 rounded hover:text-gray-600 transition-colors">
-        <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" /></svg>
+      <a href="cart.php" class="hidden sm:block relative text-gray-400 border p-2 rounded hover:text-gray-600 transition-colors"> 🛒
+        <!-- <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" /></svg> -->
         <?php
         $cartCount = 0;
         if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
