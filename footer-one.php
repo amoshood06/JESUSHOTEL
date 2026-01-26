9e59db9c38a02df17916cce418250dabd8cb2250
amoshood06 <amoshood06@gmail.com>
2026-01-26 10:38:19 +0100

    jeweewyweyu

--- footer-one.php
+++ footer-one.php
@@ -68,7 +68,7 @@
       </div>
 
       <div class="space-y-6">
-        <h4 class="text-lg font-bold uppercase tracking-widest">Rooms and Suites</h4>
+        <h4 class="text-lg font-bold uppercase tracking-widest">Rooms</h4>
         <ul class="space-y-4 text-sm text-gray-400">
           <li><a href="standard-rooms.php" class="hover:text-[#D48255] transition-colors">Standard Room</a></li>
           <li><a href="deluxe-rooms.php" class="hover:text-[#D48255] transition-colors">Deluxe Room</a></li>
