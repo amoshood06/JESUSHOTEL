ac3427e6ff45794369df6fd1f7a4de5530f3f334
amoshood06 <amoshood06@gmail.com>
2026-01-27 15:35:47 +0100

    iweuiewi

--- config/database.php
+++ config/database.php
@@ -6,9 +6,9 @@
 // define('DB_PASS', '');
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'avillaok_hotel_booking');
-define('DB_USER', 'avillaok_hotel_booking');
-define('DB_PASS', 'avillaok_hotel_booking');
+define('DB_NAME', 'dvilla_okada');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 
 // Create database connection
